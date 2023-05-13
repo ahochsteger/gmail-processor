6b77d3c66f29c0b01c061469b268dcb60f79225e
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2023-05-13 15:31:36 +0200

    test: update example test

--- test/examples/Example-v1-example-01.spec.ts
+++ test/examples/Example-v1-example-01.spec.ts
@@ -1,62 +1,52 @@
 import { RunMode } from "../../src/Context"
-import { Config } from "../../src/config/Config"
-import { GmailProcessor } from "../../src/processors/GmailProcessor"
-import { MockFactory, Mocks } from "../mocks/MockFactory"
+import { GMail2GDrive } from "../mocks/Examples"
 
-let mocks: Mocks
-let gmailProcessor: GmailProcessor
+const config = {
+  globalFilter: "has:attachment -in:trash -in:drafts -in:spam",
+  processedLabel: "gmail2gdrive/client-test",
+  sleepTime: 100,
+  maxRuntime: 280,
+  newerThan: "1d",
+  timezone: "GMT",
+  rules: [
+    {
+      filter: "to:my.name+scans@gmail.com",
+      folder: "'Scans'-yyyy-MM-dd",
+    },
+    {
+      filter: "from:example1@example.com",
+      folder: "'Examples/example1'",
+    },
+    {
+      filter: "from:example2@example.com",
+      folder: "'Examples/example2'",
+      filenameFromRegexp: ".*.pdf$",
+    },
+    {
+      filter: "(from:example3a@example.com OR from:example3b@example.com)",
+      folder: "'Examples/example3ab'",
+      filenameTo: "'file-'yyyy-MM-dd-'%s.txt'",
+      archive: true,
+    },
+    {
+      filter: "label:PDF",
+      saveThreadPDF: true,
+      folder: "PDF Emails",
+    },
+    {
+      filter: "from:example4@example.com",
+      folder: "'Examples/example4'",
+      filenameFrom: "file.txt",
+      filenameTo: "'file-'yyyy-MM-dd-'%s.txt'",
+    },
+  ],
+}
 
-beforeEach(() => {
-  mocks = MockFactory.newMocks(new Config(), RunMode.SAFE_MODE)
-  gmailProcessor = new GmailProcessor()
+it("should process a v1 config example", () => {
+  GMail2GDrive.Lib.runWithV1Config(config, RunMode.DRY_RUN)
 })
 
-describe("v1config", () => {
-  it("should process v1 config", () => {
-    const configJson = {
-      globalFilter: "has:attachment -in:trash -in:drafts -in:spam",
-      processedLabel: "gmail2gdrive/client-test",
-      sleepTime: 100,
-      maxRuntime: 280,
-      newerThan: "1d",
-      timezone: "UTC",
-      rules: [
-        {
-          filter: "to:my.name+scans@gmail.com",
-          folder: "'Scans'-yyyy-MM-dd",
-        },
-        {
-          filter: "from:example1@example.com",
-          folder: "'Examples/example1'",
-        },
-        {
-          filter: "from:example2@example.com",
-          folder: "'Examples/example2'",
-          filenameFromRegexp: ".*.pdf$",
-        },
-        {
-          filter: "(from:example3a@example.com OR from:example3b@example.com)",
-          folder: "'Examples/example3ab'",
-          filenameTo: "'file-'yyyy-MM-dd-'%s.txt'",
-          archive: true,
-        },
-        {
-          filter: "label:PDF",
-          saveThreadPDF: true,
-          folder: "PDF Emails",
-        },
-        {
-          filter: "from:example4@example.com",
-          folder: "'Examples/example4'",
-          filenameFrom: "file.txt",
-          filenameTo: "'file-'yyyy-MM-dd-'%s.txt'",
-        },
-      ],
-    }
-    gmailProcessor.runWithV1ConfigJson(
-      mocks.envContext,
-      configJson,
-      RunMode.DRY_RUN,
-    )
-  })
+it("should process a v1 config example", () => {
+  const actualConfig = GMail2GDrive.Lib.getEffectiveConfigV1(config)
+  expect(actualConfig.settings.processedLabel).toEqual(config.processedLabel)
 })
