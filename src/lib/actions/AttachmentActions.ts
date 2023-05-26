407e059c415d3ba0477b6b8d3d87e30cabd7b03e
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2023-05-26 08:55:11 +0200

    chore: eliminate obsolete code (MyMessageActions)

--- src/lib/actions/AttachmentActions.ts
+++ src/lib/actions/AttachmentActions.ts
@@ -1,5 +1,5 @@
 import { ConflictStrategy } from "../adapter/GDriveAdapter"
-import { AttachmentContext, MessageContext } from "../Context"
+import { AttachmentContext } from "../Context"
 import { writingAction } from "../utils/Decorators"
 import { PatternUtil } from "../utils/PatternUtil"
 import {
@@ -9,26 +9,6 @@ import {
   ActionReturnType,
   typedArgs,
 } from "./ActionRegistry"
-
-export class MyMessageActions implements ActionProvider<MessageContext> {
-  [key: string]: ActionFunction<MessageContext>
-  @writingAction()
-  public static forward<TArgs extends { to: string }>(
-    context: MessageContext,
-    args: ActionArgsType,
-  ): ActionReturnType {
-    const a = typedArgs<TArgs>(args)
-    return {
-      message: context.proc.gmailAdapter.messageForward(
-        context.message.object,
-        a.to as string,
-      ),
-    }
-  }
-}
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface MyMessageActions
-  extends Record<string, ActionFunction<MessageContext>> {}
 
 export class AttachmentActions implements ActionProvider<AttachmentContext> {
   [key: string]: ActionFunction<AttachmentContext>
