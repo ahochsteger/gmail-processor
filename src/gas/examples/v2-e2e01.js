8c213446c22186820f410ba40190f1df026bf42e
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2023-06-19 22:25:43 +0200

    test: add e2e test 01

---
+++ src/gas/examples/v2-e2e01.js
@@ -0,0 +1,58 @@
+/* global GMail2GDrive */
+
+var e2e01ConfigV2 = {
+  description: "An example V2 configuration",
+  settings: {
+    maxBatchSize: 10,
+    maxRuntime: 280,
+    markProcessedMethod: "mark-message-read",
+    sleepTimeThreads: 100,
+    sleepTimeMessages: 0,
+    sleepTimeAttachments: 0,
+    timezone: "UTC",
+  },
+  global: {
+    thread: {
+      match: {
+        query: "has:attachment -in:trash -in:drafts -in:spam",
+        maxMessageCount: -1,
+        minMessageCount: 1,
+        newerThan: "1d",
+      },
+      actions: [],
+    },
+  },
+  threads: [
+    {
+      match: {
+        query:
+          "from:${user.email} to:${user.email} subject:'GMail2GDrive-Test'",
+      },
+      attachments: [
+        {
+          match: {
+            name: "^(.+).png$",
+          },
+          actions: [
+            {
+              name: "attachment.store",
+              args: {
+                location:
+                  "/GMail2GDrive-Tests/v2/e2e01/${attachment.name.match.1}-stored.png",
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
+function e2e01EffectiveConfig() {
+  const effectiveConfig = GMail2GDrive.Lib.getEffectiveConfig(e2e01ConfigV2)
+  console.log(JSON.stringify(effectiveConfig, null, 2))
+}
+
+function e2e01Run() {
+  GMail2GDrive.Lib.run(e2e01ConfigV2, "dry-run")
+}
---
+++ src/test/examples/v2-e2e01.spec.ts
@@ -0,0 +1,69 @@
+import { Config } from "../../lib/config/Config"
+import { PartialDeep } from "type-fest"
+import { ProcessingConfig } from "../../lib/config/Config"
+import { ContextMocks } from "../mocks/ContextMocks"
+import { GMail2GDrive } from "../mocks/Examples"
+
+const e2e01ConfigV2 = {
+  description: "An example V2 configuration",
+  settings: {
+    maxBatchSize: 10,
+    maxRuntime: 280,
+    markProcessedMethod: "mark-message-read",
+    sleepTimeThreads: 100,
+    sleepTimeMessages: 0,
+    sleepTimeAttachments: 0,
+    timezone: "UTC",
+  },
+  global: {
+    thread: {
+      match: {
+        query: "has:attachment -in:trash -in:drafts -in:spam",
+        maxMessageCount: -1,
+        minMessageCount: 1,
+        newerThan: "1d",
+      },
+      actions: [],
+    },
+  },
+  threads: [
+    {
+      match: {
+        query:
+          "from:${user.email} to:${user.email} subject:'GMail2GDrive-Test'",
+      },
+      attachments: [
+        {
+          match: {
+            name: "^(.+).png$",
+          },
+          actions: [
+            {
+              name: "attachment.store",
+              args: {
+                location:
+                  "/GMail2GDrive-Tests/v2/e2e01/${attachment.name.match.1}-stored.png",
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
+it("should provide the effective config of v2 example e2e01", () => {
+  const effectiveConfig = GMail2GDrive.Lib.getEffectiveConfig(
+    e2e01ConfigV2 as PartialDeep<Config>,
+  )
+  expect(effectiveConfig).toBeInstanceOf(ProcessingConfig)
+})
+
+it("should process a v2 config example", () => {
+  const result = GMail2GDrive.Lib.run(
+    e2e01ConfigV2 as PartialDeep<Config>,
+    "dry-run",
+    ContextMocks.newEnvContextMock(),
+  )
+  expect(result.status).toEqual("ok")
+})
