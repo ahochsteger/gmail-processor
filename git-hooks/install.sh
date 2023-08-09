239cffa1c48f6e33b26ada6db4f648b1512dcad1
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2023-08-09 07:15:05 +0200

    chore: add git hooks + install script

---
+++ git-hooks/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh 
+# See https://www.sipios.com/blog-tech/tips-for-creating-stunning-software-releases-automatically
+
+cp git-hooks/commit-msg .git/hooks/ 
+
+echo "Git commit hooks successfully installed, you are ready to go!"
