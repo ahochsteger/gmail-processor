5c05eba7f3831f15a8f4ca42a753c54d3c8c4797
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2023-08-15 11:47:35 +0200

    build: ignore build dir for jest

--- jest.config.ts
+++ jest.config.ts
@@ -92,6 +92,7 @@ export default {
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: ["<rootDir>/build"],
 
   // Activates notifications for test results
   // notify: false,
