9c3bc3573ddedae263e37c60b9690a9b4a6695d4
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2023-09-14 00:01:45 +0200

    test: add jest setup for reflect-metadata

--- jest.config.ts
+++ jest.config.ts
@@ -140,6 +140,7 @@ export default {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
---
+++ jest.setup.ts
@@ -0,0 +1,2 @@
+// See https://stackoverflow.com/a/67371037/236784
+import "reflect-metadata"
