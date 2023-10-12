38233ef717dd3f153e71ad579e3f96951592c06b
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2023-10-13 01:01:49 +0200

    build: fix docs build

---
+++ scripts/build-docs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# shellcheck disable=SC2016
+
+set -euo pipefail
+
+cat <(
+  echo -e \"---\\nid: changelog\\nsidebar_position: 73\\n---\"
+) CHANGELOG.md >docs/docs/community/CHANGELOG.md
+cd docs
+npm run build
