b6546d76a1786e888737438f2d4d93f3048ef4bc
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2023-11-02 00:47:48 +0100

    build(deps): update all deps, fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -93,7 +93,7 @@ export default {
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
-  modulePathIgnorePatterns: ["<rootDir>/build"],
+  modulePathIgnorePatterns: ["<rootDir>/build", "<rootDir>/docs"],
 
   // Activates notifications for test results
   // notify: false,
