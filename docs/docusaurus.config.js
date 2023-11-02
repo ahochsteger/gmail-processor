4b90ffc31393a935e15d4a60c19897324fa5e9dc
andreas.hochsteger@gmail.com <andreas.hochsteger@gmail.com>
2023-11-02 15:23:44 +0100

    docs: add Algolia DocSearch integration

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -139,6 +139,35 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: '99BPL130HU',
+      
+        // Public API key: it is safe to commit it
+        apiKey: '7e30d3d1c2592af9255ab489d6710dad',
+      
+        indexName: 'gmail-processor',
+      
+        // Optional: see doc section below
+        contextualSearch: true,
+      
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'ahochsteger\\.github\\.io/gmail-processor',
+      
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+      
+        // Optional: Algolia search parameters
+        searchParameters: {},
+      
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+      
+        //... other Algolia params
+      },
     }),
 };
 
