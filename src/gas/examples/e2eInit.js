9c4a23223a65c6ab305b9990e63875b7f03ef0d2
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2023-11-29 07:38:07 +0100

    test: add office attachment examples to e2e tests

--- src/gas/examples/e2eInit.js
+++ src/gas/examples/e2eInit.js
@@ -110,6 +110,12 @@ export const e2eInitConfig = {
         'Test with HTML image tag:\n<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/320px-Gmail_icon_%282020%29.svg.png">',
       files: [],
     },
+    {
+      name: "04-office-attachments",
+      subject: "Test with office attachments",
+      htmlBody: "Test email with multiple office attachments.",
+      files: ["sample-docx", "sample-pptx", "sample-xlsx"],
+    },
   ],
 }
 
