fdab503641a6dd76c0b820746e488821b6113324
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2024-07-13 12:27:17 +0000

    feat: many deps updates (date-fns v3 + new fns, node v22)
    * update nodejs v20->v22
    * update date-fns v2->v3
    * add devbox packages: google-clasp, renovate
    * update .gitignore

--- src/lib/utils/DateExpression.spec.ts
+++ src/lib/utils/DateExpression.spec.ts
@@ -6,8 +6,6 @@ const timeFormat = "HH:mm:ss.SSS"
 const dateTimeFormat = `${dateFormat} ${timeFormat}`
 const dateStr = "2024-02-13" // NOTE: Should not be start or end of week
 const timeStr = "12:34:56.789"
-const endTimeStr = "23:59:59.999"
-const startTimeStr = "00:00:00.000"
 const dateTimeStr = `${dateStr} ${timeStr}`
 const isoDateTimeStr = `${dateStr}T${timeStr}`
 const date = new Date(isoDateTimeStr)
@@ -42,7 +40,7 @@ describe("Date Placeholder", () => {
         `${offset}:${dateTimeFormat}`,
         date,
       ),
-    ).toEqual(`2024-02-29 ${startTimeStr}`)
+    ).toEqual(`2024-02-29 00:00:00.000`)
   })
   it("should handle modifier 'offset-format' with lastDayOfMonth and relative date expression", () => {
     const offset = "lastDayOfMonth-2d"
@@ -52,30 +50,29 @@ describe("Date Placeholder", () => {
         `${offset}:${dateTimeFormat}`,
         date,
       ),
-    ).toEqual(`2024-02-27 ${startTimeStr}`)
+    ).toEqual(`2024-02-27 00:00:00.000`)
   })
   it("should handle date-fns expressions", () => {
     const expected = {
-      endOfToday: `2024-02-13 ${endTimeStr}`,
-      endOfTomorrow: `2024-02-14 ${endTimeStr}`,
-      endOfYesterday: `2024-02-12 ${endTimeStr}`,
-      startOfToday: `2024-02-13 ${startTimeStr}`,
-      startOfTomorrow: `2024-02-14 ${startTimeStr}`,
-      startOfYesterday: `2024-02-12 ${startTimeStr}`,
-      endOfDay: `${dateStr} ${endTimeStr}`,
+      endOfDay: `${dateStr} 23:59:59.999`,
+      endOfDecade: "2029-12-31 23:59:59.999",
       endOfHour: `${dateStr} 12:59:59.999`,
-      endOfISOWeek: `2024-02-18 ${endTimeStr}`,
-      endOfISOWeekYear: `2024-12-29 ${endTimeStr}`,
+      endOfISOWeek: `2024-02-18 23:59:59.999`,
+      endOfISOWeekYear: `2024-12-29 23:59:59.999`,
       endOfMinute: `${dateStr} 12:34:59.999`,
-      endOfMonth: `2024-02-29 ${endTimeStr}`,
-      endOfQuarter: `2024-03-31 ${endTimeStr}`,
+      endOfMonth: `2024-02-29 23:59:59.999`,
+      endOfQuarter: `2024-03-31 23:59:59.999`,
       endOfSecond: `${dateStr} 12:34:56.999`,
-      endOfYear: `2024-12-31 ${endTimeStr}`,
-      lastDayOfDecade: `2029-12-31 ${startTimeStr}`,
-      lastDayOfISOWeek: `2024-02-18 ${startTimeStr}`,
-      lastDayOfISOWeekYear: `2024-12-29 ${startTimeStr}`,
-      lastDayOfMonth: `2024-02-29 ${startTimeStr}`,
-      lastDayOfYear: `2024-12-31 ${startTimeStr}`,
+      endOfToday: `2024-02-13 23:59:59.999`,
+      endOfTomorrow: `2024-02-14 23:59:59.999`,
+      endOfYear: `2024-12-31 23:59:59.999`,
+      endOfYesterday: `2024-02-12 23:59:59.999`,
+      lastDayOfDecade: `2029-12-31 00:00:00.000`,
+      lastDayOfISOWeek: `2024-02-18 00:00:00.000`,
+      lastDayOfISOWeekYear: `2024-12-29 00:00:00.000`,
+      lastDayOfMonth: `2024-02-29 00:00:00.000`,
+      lastDayOfQuarter: "2024-03-31 00:00:00.000",
+      lastDayOfYear: `2024-12-31 00:00:00.000`,
       nextFriday: `2024-02-16 ${timeStr}`,
       nextMonday: `2024-02-19 ${timeStr}`,
       nextSaturday: `2024-02-17 ${timeStr}`,
@@ -90,16 +87,19 @@ describe("Date Placeholder", () => {
       previousThursday: `2024-02-08 ${timeStr}`,
       previousTuesday: `2024-02-06 ${timeStr}`,
       previousWednesday: `2024-02-07 ${timeStr}`,
-      startOfDay: `${dateStr} ${startTimeStr}`,
-      startOfDecade: `2020-01-01 ${startTimeStr}`,
+      startOfDay: `${dateStr} 00:00:00.000`,
+      startOfDecade: `2020-01-01 00:00:00.000`,
       startOfHour: `${dateStr} 12:00:00.000`,
-      startOfISOWeek: `2024-02-12 ${startTimeStr}`,
-      startOfISOWeekYear: `2024-01-01 ${startTimeStr}`,
+      startOfISOWeek: `2024-02-12 00:00:00.000`,
+      startOfISOWeekYear: `2024-01-01 00:00:00.000`,
       startOfMinute: `${dateStr} 12:34:00.000`,
-      startOfMonth: `2024-02-01 ${startTimeStr}`,
-      startOfQuarter: `2024-01-01 ${startTimeStr}`,
+      startOfMonth: `2024-02-01 00:00:00.000`,
+      startOfQuarter: `2024-01-01 00:00:00.000`,
       startOfSecond: `${dateStr} 12:34:56.000`,
-      startOfYear: `2024-01-01 ${startTimeStr}`,
+      startOfToday: `2024-02-13 00:00:00.000`,
+      startOfTomorrow: `2024-02-14 00:00:00.000`,
+      startOfYear: `2024-01-01 00:00:00.000`,
+      startOfYesterday: `2024-02-12 00:00:00.000`,
     }
     const actual: Partial<typeof expected> = {}
     Object.keys(expected).forEach((k) => {
--- src/lib/utils/DateExpression.ts
+++ src/lib/utils/DateExpression.ts
@@ -1,6 +1,7 @@
 import {
   addMilliseconds,
   endOfDay,
+  endOfDecade,
   endOfHour,
   endOfISOWeek,
   endOfISOWeekYear,
@@ -17,6 +18,7 @@ import {
   lastDayOfISOWeek,
   lastDayOfISOWeekYear,
   lastDayOfMonth,
+  lastDayOfQuarter,
   lastDayOfYear,
   nextFriday,
   nextMonday,
@@ -56,14 +58,28 @@ export type ExprInfoType = {
   type: "mod" | "new"
   fn: ModDateType | NewDateType
 }
+
+/*
+// Use this script to extract all supported date functions:
+(
+  cat node_modules/date-fns/*.d.ts \
+  | grep -E -A 2 '^export declare function [a-zA-Z0-9]+<DateType extends Date>' \
+  | grep -E -B 2 '^\): DateType;' \
+  | grep -E '^export declare function' \
+  | sed -re 's/export declare function ([^<]+)<.+/\1/g' \
+  | grep -E -v '^(toDate|constructNow)$' \
+  | sort \
+  | awk '{print "  "$1": { type: \"mod\", fn: "$1" },"}'
+  cat node_modules/date-fns/*.d.ts \
+  | grep -E '^export declare function .+\(\): Date;$' \
+  | sed -re 's/export declare function ([^<]+)\(\).+/\1/g' \
+  | sort \
+  | awk '{print "  "$1": { type: \"new\", fn: "$1" },"}'
+) | sort
+*/
 export const DATE_FNS_FUNCTIONS: Record<string, ExprInfoType> = {
-  endOfToday: { type: "new", fn: endOfToday },
-  endOfTomorrow: { type: "new", fn: endOfTomorrow },
-  endOfYesterday: { type: "new", fn: endOfYesterday },
-  startOfToday: { type: "new", fn: startOfToday },
-  startOfTomorrow: { type: "new", fn: startOfTomorrow },
-  startOfYesterday: { type: "new", fn: startOfYesterday },
   endOfDay: { type: "mod", fn: endOfDay },
+  endOfDecade: { type: "mod", fn: endOfDecade },
   endOfHour: { type: "mod", fn: endOfHour },
   endOfISOWeek: { type: "mod", fn: endOfISOWeek },
   endOfISOWeekYear: { type: "mod", fn: endOfISOWeekYear },
@@ -71,11 +87,15 @@ export const DATE_FNS_FUNCTIONS: Record<string, ExprInfoType> = {
   endOfMonth: { type: "mod", fn: endOfMonth },
   endOfQuarter: { type: "mod", fn: endOfQuarter },
   endOfSecond: { type: "mod", fn: endOfSecond },
+  endOfToday: { type: "new", fn: endOfToday },
+  endOfTomorrow: { type: "new", fn: endOfTomorrow },
   endOfYear: { type: "mod", fn: endOfYear },
+  endOfYesterday: { type: "new", fn: endOfYesterday },
   lastDayOfDecade: { type: "mod", fn: lastDayOfDecade },
   lastDayOfISOWeek: { type: "mod", fn: lastDayOfISOWeek },
   lastDayOfISOWeekYear: { type: "mod", fn: lastDayOfISOWeekYear },
   lastDayOfMonth: { type: "mod", fn: lastDayOfMonth },
+  lastDayOfQuarter: { type: "mod", fn: lastDayOfQuarter },
   lastDayOfYear: { type: "mod", fn: lastDayOfYear },
   nextFriday: { type: "mod", fn: nextFriday },
   nextMonday: { type: "mod", fn: nextMonday },
@@ -100,7 +120,10 @@ export const DATE_FNS_FUNCTIONS: Record<string, ExprInfoType> = {
   startOfMonth: { type: "mod", fn: startOfMonth },
   startOfQuarter: { type: "mod", fn: startOfQuarter },
   startOfSecond: { type: "mod", fn: startOfSecond },
+  startOfToday: { type: "new", fn: startOfToday },
+  startOfTomorrow: { type: "new", fn: startOfTomorrow },
   startOfYear: { type: "mod", fn: startOfYear },
+  startOfYesterday: { type: "new", fn: startOfYesterday },
 }
 
 export class DateExpression {
@@ -122,8 +145,7 @@ export class DateExpression {
     const expression = args[0] ?? ""
     formatStr = args[1] ?? defaultDateFormat
     dateTime = DateExpression.parseExpression(value, expression)
-    const stringValue = format(dateTime, formatStr)
-    return stringValue
+    return format(dateTime, formatStr)
   }
 
   public static normalizeExpression(
@@ -165,7 +187,6 @@ export class DateExpression {
       }
       expr = expr.slice(baseExpr.length)
     }
-    value = addMilliseconds(value, parse(expr) ?? 0)
-    return value
+    return addMilliseconds(value, parse(expr) ?? 0)
   }
 }
