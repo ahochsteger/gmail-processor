4376bc2ad1ad0508e9958a0bdd83f343e722ec1d
MikeDabrowski <mike2060@gmail.com>
2024-08-20 07:20:28 +0200

    wip: cr changes

--- src/lib/utils/PdfLibUtils.ts
+++ src/lib/utils/PdfLibUtils.ts
@@ -1,10 +1,11 @@
 import { PDFDocument } from "@cantoo/pdf-lib"
+import { AttachmentContext } from "lib/Context"
 
-export async function getDecryptedPdf(processedFileObject: GoogleAppsScript.Gmail.GmailAttachment, password: string) {
+export async function getDecryptedPdf(processedFileObject: GoogleAppsScript.Gmail.GmailAttachment, password: string, context: AttachmentContext) {
   const bytes = processedFileObject.getBytes();
-  const fileBase64 = Utilities.base64Encode(bytes);
+  const fileBase64 = context.env.utilities.base64Encode(bytes);
   const pdfDoc = await PDFDocument.load(fileBase64, { password, ignoreEncryption: true});
   const unencrypted = await pdfDoc.save() as unknown as GoogleAppsScript.Byte[];
   // the file that is stored is renamed later by the storeAttachment method
-  return Utilities.newBlob(unencrypted, 'application/pdf', 'temp_decrypted_name.pdf') as unknown as GoogleAppsScript.Gmail.GmailAttachment;
+  return context.env.utilities.newBlob(unencrypted, 'application/pdf', 'temp_decrypted_name.pdf') as unknown as GoogleAppsScript.Gmail.GmailAttachment;
 }
