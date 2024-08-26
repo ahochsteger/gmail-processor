c0ef202c636264e7cee90e6818b9fe7d6d1c7988
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2024-08-26 20:22:05 +0000

    chore(deps): eslint update config changes

---
+++ eslint.config.mjs
@@ -0,0 +1,90 @@
+// @ts-check
+
+import googleappsscript from "eslint-plugin-googleappsscript"
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
+import eslintPluginTsdoc from "eslint-plugin-tsdoc"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default tseslint.config(
+  // global ignores
+  {
+    ignores: [
+      "bak/",
+      "build/",
+      "docs/",
+      "dist/",
+      "gas/",
+      "node_modules/",
+      "*.bak*",
+      "**/*.bak*",
+      "./src/**/*.ts",
+    ],
+  },
+
+  // applies to all ts files
+  {
+    name: "ts",
+    files: ["src/lib/**/*.ts"],
+    ignores: ["src/lib/**/*.spec.ts"],
+    // ignores: ["src/examples/**/*.ts"],
+    extends: [
+      // ...eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      // ...tseslint.configs.recommendedTypeChecked,
+      //...tseslint.configs.strictTypeChecked,
+      eslintPluginPrettierRecommended,
+    ],
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      "eslint-plugin-tsdoc": eslintPluginTsdoc,
+      // prettier: eslintPluginPrettierRecommended,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.es2015,
+        ...globals.node,
+        ...googleappsscript.environments.googleappsscript.globals,
+        ...globals.jest,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-extraneous-class": "warn",
+      "@typescript-eslint/no-unnecessary-condition": "warn",
+      "@typescript-eslint/restrict-template-expressions": "warn",
+    },
+  },
+
+  // {
+  //   ...eslint.configs.recommended,
+  //   ...eslintPluginPrettierRecommended,
+  //   name: "gas",
+  //   files: ["./src/gas/**/*.js"],
+  //   extends: [],
+  //   languageOptions: {
+  //     globals: {
+  //       ...globals.es2015,
+  //       ...googleappsscript.environments.googleappsscript.globals,
+  //     },
+  //   },
+  //   rules: {},
+  // },
+
+  // global variables, applies to everything
+  // {
+  //   languageOptions: {
+  //     globals: {
+  //       ...globals.es2015,
+  //       ...globals.node,
+  //       ...googleappsscript.environments.googleappsscript.globals,
+  //       ...globals.jest,
+  //     },
+  //   },
+  // },
+)
