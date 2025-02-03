78e8fee8f8081636a7fae056d15bf4ad1b395190
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2025-02-03 16:00:49 +0000

    chore: fix warning - use NOTE instead of TODO

--- src/lib/adapter/GmailAdapter.ts
+++ src/lib/adapter/GmailAdapter.ts
@@ -92,7 +92,7 @@ export class GmailAdapter extends BaseAdapter {
       throw new Error(`Invalid label name: '${labelName}'`)
     }
     let label: GoogleAppsScript.Gmail.GmailLabel =
-      this.ctx.env.gmailApp.getUserLabelByName(labelName) // TODO: Wrong typedef - getUserLabelByName() may return null in reality!
+      this.ctx.env.gmailApp.getUserLabelByName(labelName) // NOTE: Type issue in upstream typedef - getUserLabelByName() may return null in reality!
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     if (!label) {
       this.ctx.log.info(`Creating non-existing label '${labelName}' ...`)
