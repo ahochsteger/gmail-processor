b2ddf749c9a30728b0ff6d8a4c7f55390a7ec3d2
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2025-07-26 19:13:35 +0000

    build: switch to legacy decorators

--- docs/babel.config.js
+++ docs/babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    ["@babel/plugin-proposal-decorators", { decoratorsBeforeExport: true }],
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
     '@babel/plugin-transform-class-properties'
   ],
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
